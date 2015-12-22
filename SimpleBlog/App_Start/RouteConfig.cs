cbe827203fd864b49ee39848b37c8a9dd061e5fe
vikrant <vikrant151189@gmail.com>
2015-12-22 21:57:42 +0530

    no message

--- SimpleBlog/App_Start/RouteConfig.cs
+++ SimpleBlog/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using SimpleBlog.Controllers;
 
 namespace SimpleBlog
 {
@@ -11,9 +12,10 @@ namespace SimpleBlog
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            var namespaces = new[] {typeof(PostsController).Namespace};
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            routes.MapRoute("Login", "login", new { Controller = "Auth", action = "Login" });
-            routes.MapRoute("Home", "", new { Controller = "Posts", Action = "Index" });
+            routes.MapRoute("Login", "login", new { Controller = "Auth", action = "Login" }, namespaces);
+            routes.MapRoute("Home", "", new { Controller = "Posts", Action = "Index" }, namespaces);
             //routes.MapRoute(
             //    name: "Default",
             //    url: "{controller}/{action}/{id}",
