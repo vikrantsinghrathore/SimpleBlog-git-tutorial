9cf5c3f1d12b074c928b9572ba26905411c9dcf0
vikrant <vikrant151189@gmail.com>
2015-12-25 21:08:37 +0530

    Basic Admin Layout

--- SimpleBlog/Areas/Admin/Controllers/PostsController.cs
+++ SimpleBlog/Areas/Admin/Controllers/PostsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleBlog.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,12 +8,14 @@ using System.Web.Mvc;
 namespace SimpleBlog.Areas.Admin.Controllers
 {
     [Authorize(Roles = "admin")]
+    [SelectedTab("posts")]
     public class PostsController : Controller
     {
         // GET: Admin/Posts
         public ActionResult Index()
         {
-            return Content("ADMIN POSTS!!!");
+            return View();
         }
+
     }
 }
--- SimpleBlog/Areas/Admin/Controllers/UsersController.cs
+++ SimpleBlog/Areas/Admin/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleBlog.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,12 +8,13 @@ using System.Web.Mvc;
 namespace SimpleBlog.Areas.Admin.Controllers
 {
     [Authorize(Roles = "admin")]
+    [SelectedTab("users")]
     public class UsersController : Controller
     {
         // GET: Admin/Users
         public ActionResult Index()
         {
-            return Content("Users!!!");
+            return View();
         }
     }
 }
